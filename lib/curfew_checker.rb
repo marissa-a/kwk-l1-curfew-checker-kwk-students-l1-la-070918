00c13efaf19b70a5ab53938586e0574680efb929
marissa-a <marissa.andrade@verizon.net>
2018-07-11 16:38:37 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,12 +1,15 @@
 def simple_curfew_checker(time)
   # code goes here
-  if x>=11
+  if time>=11
     puts "Get back to the common room!"
 end
 
 def curfew_checker(time)
   # code goes here
-
+  if time>=11
+puts "Get back to the common room!"
+else
+  puts "You are still under curfew"
 end
 
 def complex_curfew_checker(time)
